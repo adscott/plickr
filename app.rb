c74f702506fd6fd4f45bdd7c67fb6b024cbd516b
Adam Scott <adam.d.scott@gmail.com>
2014-09-20 13:40:36 +1000

    Added auth to urls.

--- app.rb
+++ app.rb
@@ -3,3 +3,14 @@ require 'sinatra'
 get '/' do
   'Hello World!'
 end
+
+get '/:digest/' do
+  valid = ENV['users']
+    .split(':')
+    .map { |user| OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), ENV['secret'], user) }
+    .include?(params[:digest])
+
+  pass unless valid
+
+  'Photos!'
+end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,8 +1,28 @@
 require './spec/spec_helper'
 
 describe 'plickr app' do
-  before { get '/' }
+  let(:route) { '/' }
+  let(:users) { 'developer:tester:cheesemaker' }
+  let(:secret) { 'foobar' }
+
+  before do
+    allow(ENV).to receive(:[]).with('users').and_return(users)
+    allow(ENV).to receive(:[]).with('secret').and_return(secret)
+    get route
+  end
   subject { last_response }
 
   it { should be_ok }
+
+  describe 'using a valid secret' do
+    let(:route) { '/972256b97bff8e21824900da996cbb34ce453571cf9e8d2bee7f29aedb98fb3a/' }
+
+    it { should be_ok }
+  end
+
+  describe 'using an invalid secret' do
+    let(:route) { '/cd76f6fc15579c01c1c60f146f4b0cd9bf8fed59ac6f178fa89a711bfb32353f/' }
+
+    it { should be_not_found }
+  end
 end
