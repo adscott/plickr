78439d5e837cbc11fbc555b10e7cdf061da5983d
Adam Scott <me@adams.co.tt>
2017-10-04 21:34:31 +1100

    Keeping it building.

--- infra/recipes/heroku.rb
+++ infra/recipes/heroku.rb
@@ -1,16 +1,8 @@
-apt_repository 'heroku-toolbelt' do
-  uri 'http://toolbelt.heroku.com/ubuntu'
-  components ['./']
-end
-
 script 'setup-heroku-toolbelt' do
   interpreter 'bash'
   user  'root'
   code <<-EOF
   set -e
-  wget -O- https://toolbelt.heroku.com/apt/release.key | apt-key add -
-  apt-get update
+  wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
   EOF
 end
-
-package 'heroku-toolbelt'
